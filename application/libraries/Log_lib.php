9841f3ccb4cc2f2778ed8e2a782aada91665efb8
cedrick macatangay <cedrickmacatangay@gmail.com>
2011-09-06 21:26:29 +0800

    Log-lib first commit

---
+++ application/libraries/Log_lib.php
@@ -0,0 +1,29 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Log_lib{
+	
+	public $CI;
+	
+	public function __construct(){
+		$this->CI =& get_instance();
+		
+	} 
+
+    public function log_user($username,$password)
+    {
+    	$query_str = "SELECT * FROM lend_admin WHERE username='$username' and password='$password'";
+    	$result = $this->CI->db->query($query_str,array($username,$password));
+    	if($result->num_rows()>0){
+    		return TRUE;
+    	}else{
+    		return FALSE;
+    	}
+    }
+    
+    public function fetch_info($id = array())
+    {
+    	return $this->CI->db->get_where('lend_admin', $id);
+    	
+    }
+}
+?>
