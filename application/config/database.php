527f2d1f9cf56e8ae8285146cdf8a0c102546294
cedrick macatangay <cedrickmacatangay@gmail.com>
2011-09-07 16:32:58 +0800

    change my database config

--- application/config/database.php
+++ application/config/database.php
@@ -48,8 +48,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'lending_admin';
-$db['default']['password'] = 'ph0ton-C@n0n44';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
 $db['default']['database'] = 'lending';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
