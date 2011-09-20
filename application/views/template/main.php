41e0d2719c4c223ac0c29cc6d22dd39853ca6019
Lamberto Guevarra <seoblogger07@gmail.com>
2011-09-20 21:50:09 +0800

    change location,menu,content variables on the template to be optional

--- application/views/template/main.php
+++ application/views/template/main.php
@@ -1,3 +1,11 @@
+<?php 
+	//initialization
+	$location = isset($location) ? $location : NULL;
+	$menu = isset($menu) ? $menu : array();
+	$content = isset($content) ? $content : NULL;
+
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN""http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
