6554100072c4d6694db69fb6f13f5f8ebd878e15
Lamberto Guevarra <seoblogger07@gmail.com>
2011-09-20 21:40:37 +0800

    fixed error message login link

--- application/hooks/user_check.php
+++ application/hooks/user_check.php
@@ -9,7 +9,7 @@ class User_check
 		
 		$sess_id = $CI->session->userdata('lend_user');
 		if (!$sess_id) {
-			show_error('You must <a href="user/login">login</a> first to continue.', 500, $heading = 'Login First');
+			show_error('You must <a href="'.base_url().'user/login">login</a> first to continue.', 500, $heading = 'Login First');
 		}
 	}
 }
