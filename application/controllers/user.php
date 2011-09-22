024f3caef79b3158d7e4c4889b5fc5346fc7fe22
Lamberto Guevarra <seoblogger07@gmail.com>
2011-09-22 15:59:07 +0800

    preserve url when session expired and redirect to login page

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -27,7 +27,11 @@ class User extends CI_Controller {
 
 				if ($login) {
 					$this->session->set_userdata('lend_user', md5($_POST['username'].config_item('encryption_key')));
-					redirect('/stats/', 'refresh');
+					if (isset($_GET['url'])) {
+						redirect('http://'.$_SERVER['SERVER_NAME'].urldecode($_GET['url']));
+					} else {
+						redirect('/stats/', 'refresh');
+					}
 				} else {
 					$this->load->view('template/main', array('content' => 'user/login', 'data' => array('error' => '<div class="error">Invalid Username/Password</div>')));
 				}
