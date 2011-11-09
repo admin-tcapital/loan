712242aec11646488d697ae0c818da25b30b7f68
cedrick macatangay <cedrickmacatangay@gmail.com>
2011-11-09 19:08:15 +0800

    update edit_user.php

--- application/views/user/edit_user.php
+++ application/views/user/edit_user.php
@@ -9,12 +9,20 @@
 			?>
 				<?php echo form_open(base_url().'user/user_update/'.$this->uri->segment(3));?>		
 					<?php 
-					$username = array(
-								 'name' => 'username',
-								 'id'	 => 'username',
-								 'value'	 =>  isset($_POST['username']) ? $_POST['username']: (isset($row->username) ? $row->username : ''),
+					$fname = array(
+								 'name' => 'fname',
+								 'id'	 => 'fname',
+								 'value'	 =>  isset($_POST['fname']) ? $_POST['fname']: (isset($row->fname) ? $row->fname : ''),
 								  'size'   => '25'
 							 );
+							 
+					$lname= array(
+								 'name' => 'lname',
+								 'id'	 => 'lname',
+								 'value'	 =>  isset($_POST['lname']) ? $_POST['lname']: (isset($row->lname) ? $row->lname : ''),
+								  'size'   => '25'
+							 );
+					
 					
 					?>
 					
@@ -29,10 +37,18 @@
 					</tr>
 					<tr>
 						<td>
-							User Name:
+							First Name:
 						</td>
 						<td>
-							 <?php echo form_input($username); ?>
+							 <?php echo form_input($fname); ?>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Last Name:
+						</td>
+						<td>
+							 <?php echo form_input($lname); ?>
 						</td>
 					</tr>
 					<tr>
