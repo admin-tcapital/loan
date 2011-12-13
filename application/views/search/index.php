6608cdd88b6572e2065e5290c375ce5cd3e37743
Lambert Guevarra <seoblogger07@gmail.com>
2011-12-14 01:03:18 +0800

    add label "Search Result" on search page

--- application/views/search/index.php
+++ application/views/search/index.php
@@ -20,49 +20,56 @@
     				}
 				?>
 				
-				<?php if (@$loans) : ?>
-	        	<table class="tablesorter" cellspacing="1">
-	        		<thead>
-	        			<tr>
-	        				<th>Loan #</th>
-	        				<th>Loan Type</th>
-	        				<th>Status</th>
-	        				<th>Borrower</th>
-	        				<th>Total Loan</th>
-	        			</tr>
-	        		</thead>
-	        		<tbody>
-	        			<?php foreach ($loans->result() as $loan) :?>
-	        			<tr>
-	        				<td><a href="<?php echo base_url(); ?>loan/view_info/?id=<?php echo $loan->borrower_loan_id;?>"><?php echo $loan->borrower_loan_id; ?></a></td>
-	        				<td><?php echo $loan->loan_name; ?></td>
-	        				<td><?php echo $loan->status; ?></td>
-	        				<td><a href="<?php echo base_url(); ?>borrower/view/?id=<?php echo $loan->fborrower_id;?>"><?php echo $loan->lname.', '.$loan->fname; ?></a></td>
-	        				<td>&#8369;<?php echo number_format($loan->loan_amount_total, 2, '.', ','); ?></td>
-	        			</tr>
-	        			<?php endforeach; ?>
-	        		</tbody>
-	        	</table>
-	        	<div class="pager">
-	        		<ul>
-	        			<li><span class="first ui-icon ui-icon-seek-first"></span></li>
-	        			<li><span class="prev first ui-icon ui-icon-seek-prev"></span></li>
-	        			<li><input class="pagedisplay" type="text"></li>
-	        			<li><span class="next first ui-icon ui-icon-seek-next"></span></li>
-	        			<li><span class="last ui-icon ui-icon-seek-end"></span></li>
-	        			<li>
-	        				<select class="pagesize">
-								
-								<option value="20" selected="selected">20</option>
-								<option value="30">30</option>
-								<option value="40">40</option>
-							</select>
-	        			</li>
-	        		</ul>
-				</div>
-	        	<?php else : ?>
-	        	No record found. Please try again
-	        	<?php endif; ?>
+				
+				<div class="frm_container">
+	        		<div class="frm_heading"><span>Search Result</span></div>
+	        		<div class="frm_inputs">
+	        			<?php if (@$loans) : ?>
+	        			<table class="tablesorter" cellspacing="1">
+			        		<thead>
+			        			<tr>
+			        				<th>Loan #</th>
+			        				<th>Loan Type</th>
+			        				<th>Status</th>
+			        				<th>Borrower</th>
+			        				<th>Total Loan</th>
+			        			</tr>
+			        		</thead>
+			        		<tbody>
+			        			<?php foreach ($loans->result() as $loan) :?>
+			        			<tr>
+			        				<td><a href="<?php echo base_url(); ?>loan/view_info/?id=<?php echo $loan->borrower_loan_id;?>"><?php echo $loan->borrower_loan_id; ?></a></td>
+			        				<td><?php echo $loan->loan_name; ?></td>
+			        				<td><?php echo $loan->status; ?></td>
+			        				<td><a href="<?php echo base_url(); ?>borrower/view/?id=<?php echo $loan->fborrower_id;?>"><?php echo $loan->lname.', '.$loan->fname; ?></a></td>
+			        				<td>&#8369;<?php echo number_format($loan->loan_amount_total, 2, '.', ','); ?></td>
+			        			</tr>
+			        			<?php endforeach; ?>
+			        		</tbody>
+			        	</table>
+			        	<div class="pager">
+			        		<ul>
+			        			<li><span class="first ui-icon ui-icon-seek-first"></span></li>
+			        			<li><span class="prev first ui-icon ui-icon-seek-prev"></span></li>
+			        			<li><input class="pagedisplay" type="text"></li>
+			        			<li><span class="next first ui-icon ui-icon-seek-next"></span></li>
+			        			<li><span class="last ui-icon ui-icon-seek-end"></span></li>
+			        			<li>
+			        				<select class="pagesize">
+										
+										<option value="20" selected="selected">20</option>
+										<option value="30">30</option>
+										<option value="40">40</option>
+									</select>
+			        			</li>
+			        		</ul>
+						</div>
+			        	<?php else : ?>
+			        	No record found. Please try again
+			        	<?php endif; ?>
+	        		</div>
+	        	</div>
+	        	
 	        </div>
 	        <div class="clearFix"></div>
 		</div>
