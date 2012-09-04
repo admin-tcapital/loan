cb47a6ab077b51639c9249555cfa8ec1be86ad38
Lamberto Guevarra <seoblogger07@gmail.com>
2012-09-04 16:05:44 +0800

    fixed pager of search result page

--- application/views/search/index.php
+++ application/views/search/index.php
@@ -25,7 +25,7 @@
 	        		<div class="frm_heading"><span>Search Result</span></div>
 	        		<div class="frm_inputs">
 	        			<?php if (@$loans) : ?>
-	        			<table class="tablesorter" cellspacing="1">
+	        			<table class="search" cellspacing="1">
 			        		<thead>
 			        			<tr>
 			        				<th>Loan #</th>
@@ -47,22 +47,18 @@
 			        			<?php endforeach; ?>
 			        		</tbody>
 			        	</table>
-			        	<div class="pager">
-			        		<ul>
-			        			<li><span class="first ui-icon ui-icon-seek-first"></span></li>
-			        			<li><span class="prev ui-icon ui-icon-seek-prev"></span></li>
-			        			<li><input class="pagedisplay" type="text"></li>
-			        			<li><span class="next ui-icon ui-icon-seek-next"></span></li>
-			        			<li><span class="last ui-icon ui-icon-seek-end"></span></li>
-			        			<li>
-			        				<select class="pagesize">
-										
-										<option value="20" selected="selected">20</option>
-										<option value="30">30</option>
-										<option value="40">40</option>
-									</select>
-			        			</li>
-			        		</ul>
+			        	<!-- pager -->
+						<div class='search_pager'>
+						    <img src='<?php echo base_url(); ?>css/tablesorter/first.png' class='first'/>
+						    <img src='<?php echo base_url(); ?>css/tablesorter/prev.png' class='prev'/>
+						    <span class='pagedisplay'></span> <!-- this can be any element, including an input -->
+						    <img src='<?php echo base_url(); ?>css/tablesorter/next.png' class='next'/>
+						    <img src='<?php echo base_url(); ?>css/tablesorter/last.png' class='last'/>
+						    <select class='pagesize'>
+						        <option value='20'>20</option>
+						        <option value='30'>30</option>
+						        <option value='40'>40</option>
+						    </select>
 						</div>
 			        	<?php else : ?>
 			        	No record found. Please try again
@@ -73,3 +69,21 @@
 	        </div>
 	        <div class="clearFix"></div>
 		</div>
+		<script type='text/javascript'>
+			$('.search').tablesorter()
+				.tablesorterPager({
+				    container: $('.search_pager'),
+				    updateArrows: true,
+				    page: 0,
+				    size: 20,
+				    fixedHeight: false,
+				    removeRows: false,
+				    cssNext: '.next',
+				    cssPrev: '.prev',
+				    cssFirst: '.first',
+				    cssLast: '.last',
+				    cssPageDisplay: '.pagedisplay',
+				    cssPageSize: '.pagesize',
+				    cssDisabled: 'disabled'
+			});
+		</script>
